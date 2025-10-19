7365487874789198887ec9f4259717395b6f2b4c
mahmoudfarweez5 <m.farweez@xware.co>
2025-10-19 09:19:33 +0300

    - Add Swagger and CORS

--- alilee_delivery_system/alilee_delivery_system/settings.py
+++ alilee_delivery_system/alilee_delivery_system/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'drf_yasg',  # Swagger
+    'corsheaders',  # CORS
 ]
 
 REST_FRAMEWORK = {
@@ -55,6 +57,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'alilee_delivery_system.urls'
@@ -128,3 +132,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
