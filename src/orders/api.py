bd5ffdfa5ded4ce2832c00fb0f6f414c07766adf
Hossam <hosamameen948@gmail.com>
2025-11-09 13:45:22 +0200

    add search and filter in order api

--- src/orders/api.py
+++ src/orders/api.py
@@ -2,9 +2,11 @@ from django.db.models import Q
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from users.models import UserRole
 from utilities.api import BaseViewSet
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Order
 from .serializers import (
@@ -17,6 +19,36 @@ from .serializers import (
 class OrderViewSet(BaseViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = OrderSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = [
+        "driver",
+        "trader",
+        "customer",
+        "delivery_zone",
+        "status",
+    ]
+
+    search_fields = [
+        "tracking_number",
+        "reference_code",
+        "customer__full_name",
+        "customer__email",
+        "customer__phone_number",
+        "trader__full_name",
+        "trader__email",
+        "trader__phone_number",
+        "driver__full_name",
+        "driver__email",
+        "driver__phone_number",
+    ]
+
+    ordering_fields = [
+        "tracking_number",
+        "reference_code",
+        "created",
+        "modified",
+    ]
+    ordering = ["-id"]
 
     def get_serializer_class(self):
         if self.action == "retrieve":
