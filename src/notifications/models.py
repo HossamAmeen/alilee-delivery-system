76155e850e473be235cbd9225cb053db6e11d2ff
Atareq <97070266+Atareq@users.noreply.github.com>
2025-12-24 00:55:30 +0200

    feature/send-notification-to-firestore (#16)
    * feature/send-notification-to-firestore
    
    * solve comments
    
    ---------
    
    Co-authored-by: Hossam <hosamameen948@gmail.com>

--- src/notifications/models.py
+++ src/notifications/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from users.models import UserAccount
 from utilities.models.abstract_base_model import AbstractBaseModel
 
+from .helpers import chunks
+
 
 class Notification(AbstractBaseModel):
     title = models.CharField(max_length=200)
@@ -14,3 +16,14 @@ class Notification(AbstractBaseModel):
 
     def __str__(self):
         return self.title
+
+    def bulk_create(self, notification_objs, **kwargs):
+        from .services import send_notification_to_firebase
+        notification_ids = []
+
+        for batch in chunks(notification_objs, 499):
+            objs = super().bulk_create(batch, **kwargs)
+            notification_ids.extend(obj.id for obj in objs)
+
+        send_notification_to_firebase(notification_ids)
+        return notification_ids
