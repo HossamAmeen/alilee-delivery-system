bd176b7a76fb9f791bf18993e4bc38c13fe31db6
Atareq <97070266+Atareq@users.noreply.github.com>
2026-01-03 15:50:37 +0200

    database configurateions (#18)

--- src/config/settings.py
+++ src/config/settings.py
@@ -118,8 +118,15 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env.str("DB_NAME"),
+        "USER": env.str("DB_USER"),
+        "PASSWORD": env.str("DB_PASSWORD"),
+        "HOST": env.str("DB_HOST"),
+        "PORT": env.int("DB_PORT"),
+        "OPTIONS": {
+            "sslmode": env.str("DB_SSLMODE", default="require"),
+        },
     }
 }
 
