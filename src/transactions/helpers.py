9c5170e903ecbfb1cbe753e140f0fea32af8841d
Hossam <hosamameen948@gmail.com>
2026-01-11 20:52:23 +0200

    update rollback

--- src/transactions/helpers.py
+++ src/transactions/helpers.py
@@ -35,6 +35,7 @@ def roll_back_order_transactions(order_id):
                 user_account_id=transaction.user_account_id,
                 amount=transaction.amount,
                 transaction_type=TransactionType.WITHDRAW,
+                is_rolled_back = True,
                 notes="مبلغ مسترجع الخاص بالطلب رقم "
                 + transaction.order.tracking_number,
                 order_id=transaction.order_id,
